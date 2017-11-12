ac97f83578ac65de4398596fa834882c37f0dd1c
tbricca <tbricca@gmail.com>
2017-11-12 14:01:31 -0800

    added bootstrap link, created busstop route, havent configured it yet

---
+++ routes/bus-routes.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+var apiUrl ='http://api.pugetsound.onebusaway.org/api/where/routes-for-location.xml?key=ec76e3f1-3c97-4f4a-a55e-447cfffa457b&lat=47.6120&lon=-122.335167'
+
+//arrival and departure for a stop
+
+router.get("/bus-routes", function(req, res, next){
+    //input location
+    // var location = req.body.location;
+    console.log("Hit the post route");
+    fetch(apiUrl)
+    .then(function(response) {
+        console.log(response);
+        if(response.ok) {
+            res.send({busRoute:response});
+            return response;
+        }
+        throw new Error("You messed up man");
+    });
+})
+
+module.exports = router;
